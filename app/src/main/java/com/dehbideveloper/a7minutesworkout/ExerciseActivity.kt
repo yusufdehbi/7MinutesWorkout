bf24c20851aef25172e115e60f8a954cecf7b172
yusufdehbi <youssefdehbi59@gmail.com>
2023-10-23 13:10:46 +0100

    first commit

---
+++ app/src/main/java/com/dehbideveloper/a7minutesworkout/ExerciseActivity.kt
@@ -0,0 +1,24 @@
+package com.dehbideveloper.a7minutesworkout
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.dehbideveloper.a7minutesworkout.databinding.ActivityExerciseBinding
+
+class ExerciseActivity : AppCompatActivity() {
+    private var binding: ActivityExerciseBinding? = null
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityExerciseBinding.inflate(layoutInflater)
+        setContentView(binding?.root)
+
+        setSupportActionBar(binding?.toolbarExercise)
+
+        if (supportActionBar != null){
+            supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        }
+
+        binding?.toolbarExercise?.setNavigationOnClickListener {
+            onBackPressed()
+        }
+    }
+}
