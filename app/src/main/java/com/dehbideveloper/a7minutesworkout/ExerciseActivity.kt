20de06c56e755db3f1f7396385dcc66f7f886a71
yusufdehbi <youssefdehbi59@gmail.com>
2023-10-26 19:56:29 +0100

    Adding the upcoming exercise text to restview

--- app/src/main/java/com/dehbideveloper/a7minutesworkout/ExerciseActivity.kt
+++ app/src/main/java/com/dehbideveloper/a7minutesworkout/ExerciseActivity.kt
@@ -44,11 +44,15 @@ class ExerciseActivity : AppCompatActivity() {
         binding?.tvExerciseName?.visibility = View.INVISIBLE
         binding?.flExerciseView?.visibility = View.INVISIBLE
         binding?.ivImage?.visibility = View.INVISIBLE
+        binding?.tvUpcomingLabel?.visibility = View.VISIBLE
+        binding?.tvUpcomingExerciseName?.visibility = View.VISIBLE
 
         if (restTimer != null){
             restTimer?.cancel()
             restProgress = 0
         }
+
+        binding?.tvUpcomingExerciseName?.text = exerciseList!![currentExercisePosition + 1].getName()
 
         setRestProgressBar()
     }
@@ -59,6 +63,8 @@ class ExerciseActivity : AppCompatActivity() {
         binding?.tvExerciseName?.visibility = View.VISIBLE
         binding?.flExerciseView?.visibility = View.VISIBLE
         binding?.ivImage?.visibility = View.VISIBLE
+        binding?.tvUpcomingLabel?.visibility = View.INVISIBLE
+        binding?.tvUpcomingExerciseName?.visibility = View.INVISIBLE
 
         if (exerciseTimer != null){
             exerciseTimer?.cancel()
