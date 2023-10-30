bd070db964be99c2a8c70edf5e29ba4badcc4e50
yusufdehbi <youssefdehbi59@gmail.com>
2023-10-30 11:45:33 +0100

    Displaying at witch exercise we currently are

--- app/src/main/java/com/dehbideveloper/a7minutesworkout/ExerciseActivity.kt
+++ app/src/main/java/com/dehbideveloper/a7minutesworkout/ExerciseActivity.kt
@@ -123,6 +123,8 @@ class ExerciseActivity : AppCompatActivity(), TextToSpeech.OnInitListener {
 
             override fun onFinish() {
                 currentExercisePosition++
+                exerciseList!![currentExercisePosition].setIsSelected(true)
+                exerciseAdapter!!.notifyDataSetChanged()
                 setupExerciseView()
             }
         }.start()
@@ -140,6 +142,10 @@ class ExerciseActivity : AppCompatActivity(), TextToSpeech.OnInitListener {
             }
 
             override fun onFinish() {
+                exerciseList!![currentExercisePosition].setIsSelected(false)
+                exerciseList!![currentExercisePosition].setIsCompleted(true)
+                exerciseAdapter!!.notifyDataSetChanged()
+
                 if (currentExercisePosition < exerciseList?.size!! - 1) {
                     setupRestView()
                 } else {
--- app/src/main/java/com/dehbideveloper/a7minutesworkout/ExerciseStatusAdapter.kt
+++ app/src/main/java/com/dehbideveloper/a7minutesworkout/ExerciseStatusAdapter.kt
@@ -1,7 +1,9 @@
 package com.dehbideveloper.a7minutesworkout
 
+import android.graphics.Color
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.RecyclerView
 import com.dehbideveloper.a7minutesworkout.databinding.ItemExerciseStatusBinding
 
@@ -23,6 +25,24 @@ class ExerciseStatusAdapter(val items: ArrayList<ExerciseModel>) :
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         val model: ExerciseModel = items[position]
         holder.tvItem.text = model.getId().toString()
+
+        when{
+            model.getIsSelected() -> {
+                holder.tvItem.background =
+                    ContextCompat.getDrawable(holder.itemView.context, R.drawable.item_circular_thin_color_accent_border)
+                holder.tvItem.setTextColor(Color.parseColor("#212121"))
+            }
+            model.getIsCompleted() -> {
+                holder.tvItem.background =
+                    ContextCompat.getDrawable(holder.itemView.context, R.drawable.item_circular_color_accent_background)
+                holder.tvItem.setTextColor(Color.parseColor("#FFFFFF"))
+            }
+            else -> {
+                holder.tvItem.background =
+                    ContextCompat.getDrawable(holder.itemView.context, R.drawable.item_circular_color_gray_background)
+                holder.tvItem.setTextColor(Color.parseColor("#212121"))
+            }
+        }
     }
 
 
